2190f738572edf3c09f5b7ce4c9027e31ca32b7c
csharp100 <clint63012@hotmail.com>
2014-03-16 11:23:45 -0500

    Exercise 1.7 Liang Java 8E Comprehensive Version Initial Commit

---
+++ src/pkg1/pkg7/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Exercise 1.7 from the Liang Java Textbook 8E
+ * Comprehensive Version
+ */
+
+package pkg1.pkg7;
+
+/**
+ *
+ * @author Clint Sharp
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(4 * (1.0 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/9 - 1.0/11 + 1.0/13));
+        
+    }
+    
+}
